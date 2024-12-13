219be14f57d07a88cc92a67f5358fe9452b8a5dd
JeanMarcoMiranda <elecord2014@gmail.com>
2024-12-13 10:19:54 -0500

    feat: adding getVideos usecase

---
+++ app/src/main/java/com/example/digital_mentor/domain/usecase/video/GetVideosUseCase.kt
@@ -0,0 +1,28 @@
+package com.example.digital_mentor.domain.usecase.video
+
+import android.util.Log
+import com.example.digital_mentor.domain.model.Video
+import com.example.digital_mentor.domain.repository.VideoRepository
+
+class GetVideosUseCase(
+    private val repository: VideoRepository
+) {
+    suspend fun invoke(): Result<List<Video>> {
+        return try {
+            val videoResult = repository.getVideos()
+
+            videoResult.fold(
+                onSuccess = {
+                    val videos = videoResult.getOrNull() ?: throw Exception("Videos were not found")
+                    Result.success(videos)
+                },
+                onFailure = {
+                    Log.d("Videos", "these is the exception ${videoResult.exceptionOrNull()}")
+                    throw Exception(videoResult.exceptionOrNull())
+                }
+            )
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
